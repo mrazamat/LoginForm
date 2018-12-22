222e7992107a4bb0441562a64d7108af69b8c0d4
Azamat <azamat00189@mail.ru>
2018-12-22 22:22:58 +0500

    added any methods

--- src/uz/coder/login/Controller.java
+++ src/uz/coder/login/Controller.java
@@ -9,16 +9,22 @@ import static java.lang.System.out;
 
 public class Controller extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        doGet(request,response);
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _username = request.getParameter("txtusername");
         String _password = request.getParameter("txtpassword");
 
-        try{
-
-        } catch (Exception ex){
+        try {
+            if (_username != null) {
+                if(_username.equals("coder") && _password.equals("12345")){
+                    response.sendRedirect("welcome.jsp");
+                } else {
+                    out.println("Login failed. Try again");
+                }
+            }
+        } catch (Exception ex) {
             out.println("Error: " + ex.getMessage());
         }
     }
---
+++ web/welcome.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome page</title>
+    <link href="bootstrap.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<h1 class="text-center">Welcome to member page</h1>
+</body>
+</html>
